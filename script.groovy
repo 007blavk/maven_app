02a3cb4264257fe0c951e7d320424914d0596b1a
007blavk <136388917+007blavk@users.noreply.github.com>
2023-10-24 22:03:32 -0500

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,15 +1,9 @@
-def buildJar() {
+def buildApp() {
     echo "building the application..."
-    sh 'mvn package'
 } 
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t 007blavk007/blue:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push 007blavk007/blue:jma-2.0'
-    }
 } 
 
 def deployApp() {
